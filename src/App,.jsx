c1f3bee3e49c3b5d740d3ef355e6e2e78f71506a
Karan <karanparwani006@gmail.com>
2021-06-18 01:27:58 +0530

    initial Stage

---
+++ src/App,.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <div className="main_div">
+//         <h2>ABC</h2>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// const App = () => {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         userName: "Karan Parwani's",
+//         todoItems: [
+//           { action: "Buy Bread & Butter", done: false },
+//           { action: "Go to Gym", done: false },
+//           { action: "Go to shop", done: false },
+//         ],
+//         newTodo: "",
+//       };
+//     }
+//   return (
+//     <>
+//       <div className="main_div">
+//         <h2 className="text_sec">TO - DO LIST WITH REACT</h2>
+//         <div className="center_div"></div>
+//         <div className="center_div2"> </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "KARAN'S",
+      todoItems: [
+        { action: "Buy Bread & Butter", done: false },
+        { action: "Go to Gym", done: false },
+        { action: "Go to shop", done: false },
+      ],
+      newTodo: "",
+    };
+  }
+
+  updateValue = (event) => this.setState({ newTodo: event.target.value });
+
+  checkDone = (todo) =>
+    this.setState({
+      todoItems: this.state.todoItems.map((i) =>
+        i.action === todo.action ? { ...i, done: !i.done } : i
+      ),
+    });
+
+  newTodo = () => {
+    this.setState({
+      todoItems: [
+        ...this.state.todoItems,
+        { action: this.state.newTodo, done: false },
+      ],
+    });
+  };
+
+  todoRows = () =>
+    this.state.todoItems.map((i) => (
+      <tr key={i.action}>
+        <td>{i.action}</td>
+        <td>
+          <input
+            type="checkbox"
+            checked={i.done}
+            onChange={() => this.checkDone(i)}
+          />
+        </td>
+      </tr>
+    ));
+
+  render = () => (
+    <div className="main_div">
+      <h2 className="text_sec">
+        {this.state.userName} TO - DO LIST WITH REACT
+      </h2>
+      <div className="center_div">
+        <br />
+        <input type="text" className="input" placeholder="ADD ITEMS" />
+        <button className="button"> + </button>
+        <br />
+        <br />
+        <ol>
+          <li>Buy Apple</li>
+          <li>Buy Apple</li>
+          <li>Buy Apple</li>
+          <li>Buy Apple</li>
+        </ol>
+      </div>
     </div>
   );
 }
-
-export default App;
