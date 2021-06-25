3f15b271c074d3c054634f95a487e70a5d50be88
Karan <karanparwani006@gmail.com>
2021-06-25 12:58:39 +0530

    added delete button and add to list

--- src/App.js
+++ src/App.js
@@ -1,96 +1,54 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import List from "./listItem";
+// import reactDom from "react-dom";
 
-// const App = () => {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         userName: "Karan Parwani's",
-//         todoItems: [
-//           { action: "Buy Bread & Butter", done: false },
-//           { action: "Go to Gym", done: false },
-//           { action: "Go to shop", done: false },
-//         ],
-//         newTodo: "",
-//       };
-//     }
-//   return (
-//     <>
-//       <div className="main_div">
-//         <h2 className="text_sec">TO - DO LIST WITH REACT</h2>
-//         <div className="center_div"></div>
-//         <div className="center_div2"> </div>
-//       </div>
-//     </>
-//   );
-// };
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [itemslist, setItemslist] = useState([]);
 
-// export default App;
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userName: "KARAN'S",
-      todoItems: [
-        { action: "Buy Bread & Butter", done: false },
-        { action: "Go to Gym", done: false },
-        { action: "Go to shop", done: false },
-      ],
-      newTodo: "",
-    };
-  }
-
-  updateValue = (event) => this.setState({ newTodo: event.target.value });
-
-  checkDone = (todo) =>
-    this.setState({
-      todoItems: this.state.todoItems.map((i) =>
-        i.action === todo.action ? { ...i, done: !i.done } : i
-      ),
-    });
-
-  newTodo = () => {
-    this.setState({
-      todoItems: [
-        ...this.state.todoItems,
-        { action: this.state.newTodo, done: false },
-      ],
-    });
+  
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
   };
 
-  todoRows = () =>
-    this.state.todoItems.map((i) => (
-      <tr key={i.action}>
-        <td>{i.action}</td>
-        <td>
-          <input
-            type="checkbox"
-            checked={i.done}
-            onChange={() => this.checkDone(i)}
-          />
-        </td>
-      </tr>
-    ));
+  const items = () => {
+    setItemslist((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
 
-  render = () => (
-    <div className="main_div">
-      <h2 className="text_sec">
-        {this.state.userName} TO - DO LIST WITH REACT
-      </h2>
-      <div className="center_div">
-        <br />
-        <input type="text" className="input" placeholder="ADD ITEMS" />
-        <button className="button"> + </button>
-        <br />
-        <br />
-        <ol>
-          <li>Buy Apple</li>
-          <li>Buy Apple</li>
-          <li>Buy Apple</li>
-          <li>Buy Apple</li>
-        </ol>
+  return (
+    <>
+      <div className="main_div">
+        <h2 className="text_sec">TO - DO LIST WITH REACT JS</h2>
+        <div className="center_div">
+          <input
+            type="text"
+            placeholder="ADD ITEMS"
+            className="input"
+            value={inputList}
+            onChange={itemEvent}
+          />
+          <button className="button" onClick={items}>
+            {" "}
+            +{" "}
+          </button>
+          <br />
+          
+          <br />
+          <ol>
+            {itemslist.map((itemValue) => {
+              // return <li> {itemValue}</li>;
+              return <List text={itemValue} />;
+            })}
+          </ol>
+        </div>
       </div>
-    </div>
+    </>
   );
-}
+};
+
+export default App;
